6960a4f8b1fea54a8d5883ad5582fed5c027cf5e
Shevchuk-Ren <shevchuk_re@ukr.net>
2021-12-07 21:16:23 +0200

    created function for click (test)

--- src/utils/mocks/mock-user-page-table-data.tsx
+++ src/utils/mocks/mock-user-page-table-data.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import ActionButton from 'components/ActionButton';
 
+function toggle(isBlocked: boolean) {
+  if (isBlocked) {
+    /* eslint-disable no-console */
+    console.log(!isBlocked);
+    return;
+  }
+  /* eslint-disable no-console */
+  console.log(isBlocked);
+}
 /* eslint-disable no-console */
 export const mockUserPageTableColumns: any[] = [
   {
     title: 'Name',
     width: '50%',
     dataIndex: 'firstName',
-    sorter: (a: any, b: any) => a.name.length - b.name.length,
+    sorter: (a: any, b: any) => a.firstName.length - b.firstName.length,
     sortDirections: ['descend', 'ascend'],
-    render: (firstName: any) => (<span>{firstName}</span>),
+    render: (firstName: any, id: any) => (<span key={id}>{firstName}</span>),
   },
   {
     title: 'Actions',
@@ -32,7 +41,7 @@ export const mockUserPageTableColumns: any[] = [
     width: '25%',
     dataIndex: 'is_blocked',
     render: (is_blocked: any) => (
-      <ActionButton type="text" size="middle" onClick={() => console.log('Button pressed')}>{is_blocked ? 'Block' : 'Unblock' }</ActionButton>
+      <ActionButton type="text" size="middle" onClick={() => toggle(is_blocked)}>{is_blocked ? 'Block' : 'Unblock' }</ActionButton>
     ),
   },
 ];
